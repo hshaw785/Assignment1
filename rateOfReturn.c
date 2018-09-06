2191575dfe5b39dbc043ad8cf424d4c2bc7486b5
hshaw785 <42560642+hshaw785@users.noreply.github.com>
2018-09-05 20:50:40 -0500

    Create rateOfReturn.c

---
+++ rateOfReturn.c
@@ -0,0 +1,16 @@
+/*
+ * Author: Haley Shaw
+ *
+ * Date: 09/05/2018
+ *
+ * This program is for calculating the annualized return rate of an
+ * investment given the initial and final values of the investment
+ * and the time elapsed.
+ *
+ */
+ 
+ #include<stdio.h>
+ #include<stdlib.h>
+ #include<math.h>
+ 
+ int main(int argc, char **argv) {}
